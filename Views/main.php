279a60c1295b1b532fb87060860ca9fe33de2acc
Benoit VRIGNAUD <benoit.vrignaud@zaclys.net>
2018-12-09 20:10:42 +0100

    fix logo url to '/admin'

--- Views/main.php
+++ Views/main.php
@@ -36,7 +36,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/admin" class="brand-link">
       <img src="/assets/node_modules/admin-lte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
       	   class="brand-image img-circle elevation-3"
            style="opacity: .8">
